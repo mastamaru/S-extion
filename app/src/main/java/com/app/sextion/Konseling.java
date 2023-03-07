8a537f8fd1b0384cee614df66353bbfed3fe911d
Masta <muhammad.al.ghifari.fausta@mail.ugm.ac.id>
2023-03-07 14:52:34 +0700

    feat: konseling page

--- app/src/main/java/com/app/sextion/Konseling.java
+++ app/src/main/java/com/app/sextion/Konseling.java
@@ -14,12 +14,22 @@ public class Konseling extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_konseling);
 
-        //switch activity navbar
+        //switch activity ke home
         RelativeLayout HomeLayout = findViewById(R.id.buttonHome);
         HomeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Konseling.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        //switch activity ke profile
+        RelativeLayout profileLayout = findViewById(R.id.buttonProfile);
+        profileLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Konseling.this, Profile.class);
                 startActivity(intent);
             }
         });
